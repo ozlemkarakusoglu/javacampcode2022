fa08a69c8410182799177b71c4d55fb6ccb08087
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-09-19 01:40:05 +0300

    Add files via upload

---
+++ ifblok/src/ifblok/Main.java
@@ -0,0 +1,18 @@
+package ifblok;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 20 ;
+		if (sayi<20) {
+			System.out.println("sayý 20 den küçüktür.");
+		}
+		else if (sayi>20) {
+			System.out.println("sayý 20 den büyüktür.");
+		}
+		else {
+			System.out.println("sayý 20 ye eþittir.");
+	}
+
+	}
+}
