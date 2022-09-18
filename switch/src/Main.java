f9c1cf9a1100c7026719734510e4e1cdd7e72358
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-09-19 02:10:29 +0300

    Add files via upload

---
+++ switch/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'S';
+		
+		switch (grade) {
+			case 'A':
+				System.out.println("Conguralations!");
+				break;
+			case 'C':
+				System.out.println("Good");
+				break;
+			case 'F':
+				System.out.println("You are fail.");
+				break;
+			default :
+				System.out.println("Wrong letter!");
+				
+				
+		}
+
+	}
+
+}
