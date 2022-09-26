b2fed01c78fea34aa3ddd483f239b945471e25e0
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-09-26 16:37:30 +0300

    Add files via upload

---
+++ doWhile/src/doWhile/Main.java
@@ -0,0 +1,14 @@
+package doWhile;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j=1;
+		do {
+			System.out.println(j);
+			j+=2;
+		} while (j<10);
+		System.out.println("do-while döngüsü bitti.");
+	}
+
+}
