6481e0d01fa6d5fbe81c4a251416b8d033fd11e8
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-09-28 00:57:24 +0300

    Add files via upload

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,19 @@
+package sesliHarfler;
+
+public class Main {
+	public static void main(String[] args)  {
+		char harf = 'O' ;
+		switch (harf) {
+			case 'A':
+			case 'I':
+			case 'O':
+			case 'U':
+				System.out.println("Kalýn sesli harf");
+				break;
+				default:
+					System.out.println("ince sesli harf");
+				
+		}
+	}
+
+}
