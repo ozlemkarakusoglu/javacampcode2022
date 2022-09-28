59983924ed2432581ee3a997eda01dc44bb3146f
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-09-28 18:56:07 +0300

    Add files via upload

---
+++ methods/src/methods/Main.java
@@ -0,0 +1,35 @@
+package methods;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//main'de bir metoddur
+		sayiBulmaca();
+	}
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+		
+	}
+	public static void sayiBulmaca() {
+		int[] sayilar = new int[] {1,2,5,7,9,0};
+		int aranacak = 7;
+		boolean varMi=false ;
+		
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;				
+			}
+		}
+		String mesaj = "";
+		if (varMi) {
+			mesaj = "sayý mevcuttur: " +aranacak;
+			mesajVer(mesaj);
+		}else {
+			mesajVer("sayý mevcut deðildir: " +aranacak);	
+		}
+	
+	}
+	
+	
+}
