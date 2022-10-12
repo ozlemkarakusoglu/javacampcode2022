b27e72aac0f30a7bbec78e22b5049a1d89494881
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-10-13 01:27:23 +0300

    Add files via upload

---
+++ overriding/src/overriding/Main.java
@@ -0,0 +1,15 @@
+package overriding;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseKrediManager[] krediManagers = new BaseKrediManager[] {new OgretmenKrediManager(), new TarimKrediManager(), new OgrenciKrediManager()};
+		//BaseKrediManager türündeki krediManagerlar, neredeki, krediManagers taki
+		for (BaseKrediManager krediManager : krediManagers ) {
+			
+			System.out.println(krediManager.hesapla(1000));
+		}
+
+	}
+
+}
