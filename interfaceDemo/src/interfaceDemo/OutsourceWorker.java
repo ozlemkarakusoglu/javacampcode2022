24e8b40a4e697388489709c010c64a73e2d0591d
özlem karakuşoğlu <84107003+ozlemkarakusoglu@users.noreply.github.com>
2022-10-13 16:50:04 +0300

    Add files via upload

---
+++ interfaceDemo/src/interfaceDemo/OutsourceWorker.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+//dýþardan gelen çalýþan
+public class OutsourceWorker implements IWorkable {
+
+	@Override
+	public void work() {
+		
+		
+	}
+
+}
---
+++ interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,11 @@
+package interfaceDemo;
+//kendi çalýþanýmýz
+public class Worker implements IWorkable, IEatable, IPayable {
+
+	@Override
+	public void work() {
+		
+		
+	}
+
+}
